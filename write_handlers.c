a086ba9e987a7b43225edcb3ae8d99be8d652beb
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-07-26 01:49:31 +0300

    handler 3

--- write_handlers.c
+++ write_handlers.c
@@ -102,3 +102,49 @@ int write_num(int ind, char buffer[],
         buffer[--ind] = extra_c;
     return (write(1, &buffer[ind], length));
 }
+
+int write_unsgnd(int is_negative, int ind,
+                 char buffer[],
+                 int flags, int width, int precision, int size)
+{
+    /* The number is stored at the bufer's right and starts at position i */
+    int length = BUFF_SIZE - ind - 1, i = 0;
+    char padd = ' ';
+
+    UNUSED(is_negative);
+    UNUSED(size);
+
+    if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
+        return (0); /* printf(".0d", 0)  no char is printed */
+
+    if (precision > 0 && precision < length)
+        padd = ' ';
+
+    while (precision > length)
+    {
+        buffer[--ind] = '0';
+        length++;
+    }
+
+    if ((flags & F_ZERO) && !(flags & F_MINUS))
+        padd = '0';
+
+    if (width > length)
+    {
+        for (i = 0; i < width - length; i++)
+            buffer[i] = padd;
+
+        buffer[i] = '\0';
+
+        if (flags & F_MINUS) /* Asign extra char to left of buffer [buffer>padd]*/
+        {
+            return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
+        }
+        else /* Asign extra char to left of padding [padd>buffer]*/
+        {
+            return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
+        }
+    }
+
+    return (write(1, &buffer[ind], length));
+}
